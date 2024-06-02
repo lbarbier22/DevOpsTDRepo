cd430f52182e634d7708200e1292810051fe2d0e
lbarbier22 <barbier.lucas08@gmail.com>
2024-06-02 16:42:24 +0200

    feat : exo1bis done

--- script1bis.bash
+++ script1bis.bash
@@ -21,3 +21,31 @@ echo "Daredevil is a blind comic character." > personnages/comics/hommes/sans\ c
 #7
 cat personnages/comics/hommes/cape/batman personnages/comics/hommes/sans\ cape/daredevil > personnages/mascottes/mixdarbat
 
+#8 & 9
+sudo useradd -m fanboy || true
+
+#10
+sudo cp -r personnages /home/fanboy
+
+#11
+sudo chown -R fanboy:fanboy /home/fanboy/personnages
+
+#12
+sudo ln -sf /home/fanboy/personnages /home/fanboy/persofanboy
+
+#13
+cp -r personnages /home/lucas/
+sudo ln -sf /home/lucas/personnages /home/lucas/perso_lucas
+
+#14
+sudo apt install -y tree
+sudo tree /home/fanboy/personnages > 14.txt
+
+#15
+grep -v "directories" 14.txt > 15.txt
+
+#16
+history_file_path=$HISTFILE
+if [[ -f $history_file_path ]]; then
+    grep -v "cd" $history_file_path | tail -n 250 > myhistory
+fi
