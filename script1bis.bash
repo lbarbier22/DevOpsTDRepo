1be47040189adfc759130ed5c08189fd7275a739
lbarbier22 <barbier.lucas08@gmail.com>
2024-06-02 16:33:11 +0200

    feat : exo1bis partially done

---
+++ script1bis.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#1
+mv personnages/super\ heros/hommes/sans\ cape/linuxman personnages/mascottes/tux
+
+#2
+mv personnages/super\ heros personnages/comics
+
+#3
+echo "Bruce Wayne hides behind this character." > personnages/comics/hommes/cape/batman
+
+#4
+echo "He lives in Gotham." >> personnages/comics/hommes/cape/batman
+
+#5
+echo "Homer Simpson hides behind this character." > personnages/comics/hommes/sans\ cape/daredevil
+
+#6
+echo "Daredevil is a blind comic character." > personnages/comics/hommes/sans\ cape/daredevil
+
+#7
+cat personnages/comics/hommes/cape/batman personnages/comics/hommes/sans\ cape/daredevil > personnages/mascottes/mixdarbat
+
